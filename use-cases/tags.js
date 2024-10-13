2053b7270a5f0156ff374b056cf03b9100601874
Yan Vidal <ter_yvidal@maisaedu.com.br>
2024-10-12 23:44:47 -0300

    fix: sync only specific tags

--- use-cases/tags.js
+++ use-cases/tags.js
@@ -2,97 +2,145 @@ const Tag = require("../models/Tag");
 const Image = require("../models/Image");
 
 const listTags = async () => {
-    try {
-        const tags = await Tag.find().sort({ name: 1 });
-        return tags
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Erro ao listar tags" });
-    }
-}
+  try {
+    const tags = await Tag.find().sort({ name: 1 });
+    return tags;
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro ao listar tags" });
+  }
+};
 
 const uploadTag = async (payload) => {
-    try {
-        const tag = {
-            name: payload.name,
-            code: payload.code
-        };
+  try {
+    const tag = {
+      name: payload.name,
+      code: payload.code,
+    };
 
-        const newTag = new Tag(tag);
+    const newTag = new Tag(tag);
 
-        await newTag.save();
+    await newTag.save();
 
-        return newTag;
-    } catch (err) {
-        console.error(err);
-        throw new Error("Erro ao cadastrar tags");
-    }
+    return newTag;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Erro ao cadastrar tags");
+  }
 };
 
 const editTag = async (id, body) => {
-    try {
-        const tag = await Tag.findById(id);
+  try {
+    const tag = await Tag.findById(id);
 
-        if (!tag) {
-            throw new Error("Tag não encontrada");
-        }
-
-        tag.name = body.name;
-        tag.code = body.code;
-
-        const updatedTag = await tag.save();
-
-        return updatedTag;
-    } catch (err) {
-        console.error(err);
-        throw new Error("Erro ao editar tag");
+    if (!tag) {
+      throw new Error("Tag não encontrada");
     }
+
+    tag.name = body.name;
+    tag.code = body.code;
+
+    const updatedTag = await tag.save();
+
+    return updatedTag;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Erro ao editar tag");
+  }
 };
 
 const deleteTag = async (id) => {
-    const deletedDocument = await Tag.findByIdAndDelete(id);
+  const deletedDocument = await Tag.findByIdAndDelete(id);
 
-    if (!deletedDocument) {
-        throw new Error("Tag não encontrado");
-    }
+  if (!deletedDocument) {
+    throw new Error("Tag não encontrado");
+  }
 
-    return true;
+  return true;
 };
 
 const sync = async () => {
-    try {
-        const tags = await Tag.find();
+  try {
+    const tags = await Tag.find();
 
-        const tagsInDB = tags.map(tag => tag.name);
+    const tagsInDB = tags.map((tag) => tag.name);
 
-        const tagsInImages = [...new Set((await Image.find({}, { tags: 1 })).map(image => image.tags).flat())];
+    //const tagsInImages = [...new Set((await Image.find({}, { tags: 1 })).map(image => image.tags).flat())];
 
-        const tagsToRemove = tagsInDB.filter(tag => !tagsInImages.includes(tag)).map(tag =>{
-            return {
-                name: tag,
-                code: tag.toLowerCase().replaceAll(' ', '')
-            }});
+    const tagsInImages = [
+      "Aerial photography",
+      "Architecture",
+      "Art",
+      "Astronomy",
+      "Beach",
+      "Black and white",
+      "Bridge",
+      "Cave",
+      "Church",
+      "Cloud",
+      "Cyberpunk",
+      "Fog",
+      "Forest",
+      "Graffiti",
+      "Harbor",
+      "Lake",
+      "Landscape",
+      "Metro",
+      "Mountain",
+      "Museum",
+      "Nature",
+      "Neon",
+      "Night",
+      "Ocean",
+      "People",
+      "Reflection",
+      "Religious",
+      "River",
+      "Rural",
+      "Sand",
+      "Sky",
+      "Skyscraper",
+      "Statue",
+      "Sunrise",
+      "Sunset",
+      "Traffic",
+      "Twilight",
+      "Urban",
+      "Vehicle",
+    ];
 
-        const tagsToInsert = tagsInImages.filter(tag => !tagsInDB.includes(tag)).map(tag =>{
-            return {
-                name: tag,
-                code: tag.toLowerCase().replaceAll(' ', '')
-            }});
+    const tagsToRemove = tagsInDB
+      .filter((tag) => !tagsInImages.includes(tag))
+      .map((tag) => {
+        return {
+          name: tag,
+          code: tag.toLowerCase().replaceAll(" ", ""),
+        };
+      });
 
-        if (tagsToRemove.length) await Tag.deleteMany({$or: tagsToRemove});
-        if (tagsToInsert.length) await Tag.insertMany(tagsToInsert);
+    const tagsToInsert = tagsInImages
+      .filter((tag) => !tagsInDB.includes(tag))
+      .map((tag) => {
+        return {
+          name: tag,
+          code: tag.toLowerCase().replaceAll(" ", ""),
+        };
+      });
 
-        return true
-    } catch (error) {
-        console.error(error);
-        throw new Error("Erro ao sincronizar tags");
-    }
-}
+    if (tagsToRemove.length) await Tag.deleteMany({ $or: tagsToRemove });
+    if (tagsToInsert.length) await Tag.insertMany(tagsToInsert);
+
+    return true;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Erro ao sincronizar tags");
+  }
+};
 
 module.exports = {
-    listTags,
-    uploadTag,
-    editTag,
-    deleteTag,
-    sync
+  listTags,
+  uploadTag,
+  editTag,
+  deleteTag,
+  sync,
 };
